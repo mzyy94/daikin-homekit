fb3caf0858e90fdf442d21d710d4df232256d955
Yuki MIZUNO <mizuyuu0904@gmail.com>
2023-04-10 22:11:56 +0900

    Add post request test code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    match std::env::args().nth(1) {
+        Some(ip_addr) => get_status(ip_addr).await,
+        None => Ok(()),
+    }
+}
+
+async fn get_status(ip_addr: String) -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::builder()
+        .http1_title_case_headers()
+        .build()?;
+    let url = format!("http://{}/dsiot/multireq", ip_addr);
+    let resp = client.post(url).send().await?;
+
+    if resp.status() != reqwest::StatusCode::OK {
+        println!("{}", resp.status());
+        return Ok(());
+    }
+
+    let body = resp.text().await?;
+    println!("{}", body);
+
+    Ok(())
 }
