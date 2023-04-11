c34cb63c163e9c154dfbfe26059d38531f36b16c
Yuki MIZUNO <mizuyuu0904@gmail.com>
2023-04-12 08:04:54 +0900

    Add example get_status app

---
+++ examples/get_status.rs
@@ -0,0 +1,34 @@
+use clap::Parser;
+use daikin_homekit::daikin::Daikin;
+use std::net::Ipv4Addr;
+
+#[derive(Parser)]
+#[clap(
+    author = "mzyy94",
+    version = "v0.0.1",
+    about = "Get current status from Daikin AC"
+)]
+struct Cli {
+    /// IPv4 address of Daikin AC
+    #[arg(value_name = "ip_address")]
+    ip_addr: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+    let addr = cli.ip_addr.parse::<Ipv4Addr>()?;
+    get_status(addr).await
+}
+
+async fn get_status(ip_addr: Ipv4Addr) -> Result<(), Box<dyn std::error::Error>> {
+    let daikin = Daikin::new(ip_addr);
+
+    let info = daikin.get_info().await?;
+    println!("{:#?}", info);
+
+    let status = daikin.get_status().await?;
+    println!("{:#?}", status);
+
+    Ok(())
+}
