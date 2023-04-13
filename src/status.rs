3708d1e4c7056928cdb759434e7028c42d702ee2
Yuki MIZUNO <mizuyuu0904@gmail.com>
2023-04-13 20:01:33 +0900

    Add wind status

--- src/status.rs
+++ src/status.rs
@@ -3,30 +3,6 @@ use crate::request::{DaikinRequest, Request};
 use crate::response::DaikinResponse;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
-pub enum Mode {
-    Fan = 0,
-    Heating = 1,
-    Cooling = 2,
-    Auto = 3,
-    Dehumidify = 5,
-
-    Unknown = 255,
-}
-
-impl std::convert::From<u8> for Mode {
-    fn from(num: u8) -> Self {
-        match num {
-            0 => Self::Fan,
-            1 => Self::Heating,
-            2 => Self::Cooling,
-            3 => Self::Auto,
-            5 => Self::Dehumidify,
-            _ => Self::Unknown,
-        }
-    }
-}
-
 #[derive(Serialize, Deserialize, Clone, Copy, Debug)]
 pub struct DaikinStatus {
     pub power: Option<u8>,
@@ -37,6 +13,10 @@ pub struct DaikinStatus {
     pub target_cooling_temperature: Option<f32>,
     pub target_heating_temperature: Option<f32>,
     pub target_automatic_temperature: Option<f32>,
+    pub wind_speed: Option<WindSpeed>,
+    pub automode_wind_speed: Option<AutoModeWindSpeed>,
+    pub vertical_wind_direction: Option<VerticalDirection>,
+    pub horizontal_wind_direction: Option<HorizontalDirection>,
     meta: Meta,
 }
 
@@ -47,6 +27,10 @@ struct Meta {
     target_cooling_temperature: ((f32, Option<f32>, Option<f32>), usize),
     target_heating_temperature: ((f32, Option<f32>, Option<f32>), usize),
     target_automatic_temperature: ((f32, Option<f32>, Option<f32>), usize),
+    wind_speed: ((f32, Option<f32>, Option<f32>), usize),
+    automode_wind_speed: ((f32, Option<f32>, Option<f32>), usize),
+    vertical_wind_direction: ((f32, Option<f32>, Option<f32>), usize),
+    horizontal_wind_direction: ((f32, Option<f32>, Option<f32>), usize),
 }
 
 impl DaikinStatus {
@@ -61,12 +45,28 @@ impl DaikinStatus {
             target_cooling_temperature: get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_02 -> f32),
             target_heating_temperature: get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_03 -> f32),
             target_automatic_temperature: get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_1F -> f32),
+            wind_speed:
+                get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_09 -> u8)
+                    .map(|v| v.into()),
+            automode_wind_speed:
+                get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_26 -> u8)
+                    .map(|v| v.into()),
+            vertical_wind_direction:
+                get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_05 -> u8)
+                    .map(|v| v.into()),
+            horizontal_wind_direction:
+                get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_06 -> u8)
+                    .map(|v| v.into()),
             meta: Meta {
                 power: get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_A002.p_01 -> meta_size),
                 mode: get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_01 -> meta_size),
                 target_cooling_temperature: get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_02 -> meta_size),
                 target_heating_temperature: get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_03 -> meta_size),
                 target_automatic_temperature: get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_1F -> meta_size),
+                wind_speed: get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_09 -> meta_size),
+                automode_wind_speed: get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_26 -> meta_size),
+                vertical_wind_direction: get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_05 -> meta_size),
+                horizontal_wind_direction: get_prop!(response."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_06 -> meta_size),
             },
         }
     }
@@ -111,7 +111,178 @@ impl DaikinStatus {
             set_prop!(&mut req."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_1F = pv);
         };
 
+        if let Some(value) = self.wind_speed {
+            let pv = PropValue::from(
+                value as u8 as f32,
+                self.meta.wind_speed.0 .0,
+                self.meta.wind_speed.1,
+            );
+            set_prop!(&mut req."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_09 = pv)
+        };
+
+        if let Some(value) = self.automode_wind_speed {
+            let pv = PropValue::from(
+                value as u8 as f32,
+                self.meta.automode_wind_speed.0 .0,
+                self.meta.automode_wind_speed.1,
+            );
+            set_prop!(&mut req."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_26 = pv)
+        };
+
+        if let Some(value) = self.vertical_wind_direction {
+            let pv = PropValue::from(
+                value as u8 as f32,
+                self.meta.vertical_wind_direction.0 .0,
+                self.meta.vertical_wind_direction.1,
+            );
+            set_prop!(&mut req."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_05 = pv)
+        };
+
+        if let Some(value) = self.horizontal_wind_direction {
+            let pv = PropValue::from(
+                value as u8 as f32,
+                self.meta.horizontal_wind_direction.0 .0,
+                self.meta.horizontal_wind_direction.1,
+            );
+            set_prop!(&mut req."/dsiot/edge/adr_0100.dgc_status".e_1002.e_3001.p_06 = pv)
+        };
+
         req
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Mode {
+    Fan = 0,
+    Heating = 1,
+    Cooling = 2,
+    Auto = 3,
+    Dehumidify = 5,
+
+    Unknown = 255,
+}
+
+impl std::convert::From<u8> for Mode {
+    fn from(num: u8) -> Self {
+        match num {
+            0 => Self::Fan,
+            1 => Self::Heating,
+            2 => Self::Cooling,
+            3 => Self::Auto,
+            5 => Self::Dehumidify,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
+pub enum WindSpeed {
+    Silent = 0x0B,
+    Lev1 = 0x03,
+    Lev2 = 0x04,
+    Lev3 = 0x05,
+    Lev4 = 0x06,
+    Lev5 = 0x07,
+    Auto = 0x0A,
+
+    Unknown = 0xFF,
+}
+
+impl std::convert::From<u8> for WindSpeed {
+    fn from(num: u8) -> Self {
+        match num {
+            0x0B => Self::Silent,
+            0x03 => Self::Lev1,
+            0x04 => Self::Lev2,
+            0x05 => Self::Lev3,
+            0x06 => Self::Lev4,
+            0x07 => Self::Lev5,
+            0x0A => Self::Auto,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
+pub enum AutoModeWindSpeed {
+    Silent = 0x0B,
+    Auto = 0x0A,
+
+    Unknown = 0xFF,
+}
+
+impl std::convert::From<u8> for AutoModeWindSpeed {
+    fn from(num: u8) -> Self {
+        match num {
+            0x0B => Self::Silent,
+            0x0A => Self::Auto,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
+pub enum VerticalDirection {
+    TopMost = 0x01,
+    Top = 0x02,
+    Center = 0x03,
+    Bottom = 0x04,
+    BottomMost = 0x05,
+
+    Swing = 0x0F,
+    Auto = 0x10,
+
+    Nice = 0x17,
+
+    Unknown = 0xFF,
+}
+
+impl std::convert::From<u8> for VerticalDirection {
+    fn from(num: u8) -> Self {
+        match num {
+            0x01 => Self::TopMost,
+            0x02 => Self::Top,
+            0x03 => Self::Center,
+            0x04 => Self::Bottom,
+            0x05 => Self::BottomMost,
+            0x0F => Self::Swing,
+            0x10 => Self::Auto,
+            0x17 => Self::Nice,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
+pub enum HorizontalDirection {
+    LeftMost = 0x02,
+    Left = 0x03,
+    LeftCenter = 0x04,
+    Center = 0x05,
+    RightCenter = 0x06,
+    Right = 0x07,
+    RightMost = 0x08,
+
+    Swing = 0x0F,
+    Auto = 0x10,
+
+    Unknown = 0xFF,
+}
+
+impl std::convert::From<u8> for HorizontalDirection {
+    fn from(num: u8) -> Self {
+        match num {
+            0x02 => Self::LeftMost,
+            0x03 => Self::Left,
+            0x04 => Self::LeftCenter,
+            0x05 => Self::Center,
+            0x06 => Self::RightCenter,
+            0x07 => Self::Right,
+            0x08 => Self::RightMost,
+            0x0F => Self::Swing,
+            0x10 => Self::Auto,
+            _ => Self::Unknown,
+        }
     }
 }
 
@@ -133,6 +304,10 @@ mod tests {
         assert_eq!(status.target_cooling_temperature, Some(24.5));
         assert_eq!(status.target_heating_temperature, Some(25.0));
         assert_eq!(status.target_automatic_temperature, Some(0.0));
+        assert_eq!(status.wind_speed, Some(WindSpeed::Auto));
+        assert_eq!(status.automode_wind_speed, Some(AutoModeWindSpeed::Auto));
+        assert_eq!(status.vertical_wind_direction, Some(VerticalDirection::Auto));
+        assert_eq!(status.horizontal_wind_direction, Some(HorizontalDirection::Auto));
     }
 
     #[test]
@@ -146,6 +321,10 @@ mod tests {
         status.target_cooling_temperature = Some(24.5);
         status.target_heating_temperature = Some(25.0);
         status.target_automatic_temperature = Some(0.0);
+        status.automode_wind_speed = Some(AutoModeWindSpeed::Silent);
+        status.wind_speed = Some(WindSpeed::Lev4);
+        status.vertical_wind_direction = Some(VerticalDirection::BottomMost);
+        status.horizontal_wind_direction = Some(HorizontalDirection::RightCenter);
 
         let req = status.build_request();
         let json = serde_json::to_value(&req).unwrap();
@@ -153,6 +332,18 @@ mod tests {
             json,
             serde_json::from_str::<serde_json::Value>(include_str!("./fixtures/update.json"))
                 .unwrap()
+        );
+    }
+
+    #[test]
+    fn debug_display() {
+        let res: DaikinResponse = serde_json::from_str(include_str!("./fixtures/status.json"))
+            .expect("Invalid JSON file.");
+        let status = DaikinStatus::new(res);
+
+        assert_eq!(
+            format!("{:?}", status),
+            r#"DaikinStatus { power: Some(0), current_temperature: Some(20.0), current_humidity: Some(50.0), current_outside_temperature: Some(19.0), mode: Some(Cooling), target_cooling_temperature: Some(24.5), target_heating_temperature: Some(25.0), target_automatic_temperature: Some(0.0), wind_speed: Some(Auto), automode_wind_speed: Some(Auto), vertical_wind_direction: Some(Auto), horizontal_wind_direction: Some(Auto), meta: Meta { power: ((1.0, Some(0.0), Some(1.0)), 2), mode: ((0.0, None, Some(47.0)), 4), target_cooling_temperature: ((0.5, Some(18.0), Some(32.0)), 2), target_heating_temperature: ((0.5, Some(14.0), Some(30.0)), 2), target_automatic_temperature: ((0.5, Some(-5.0), Some(5.0)), 2), wind_speed: ((0.0, None, Some(3320.0)), 4), automode_wind_speed: ((0.0, None, Some(3072.0)), 4), vertical_wind_direction: ((0.0, None, Some(8486975.0)), 8), horizontal_wind_direction: ((0.0, None, Some(98813.0)), 6) } }"#
         );
     }
 }
