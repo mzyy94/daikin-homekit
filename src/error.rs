a3c46f15236ea5442443c23cdee00492529464e3
Yuki MIZUNO <mizuyuu0904@gmail.com>
2023-04-15 13:21:47 +0900

    Add thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,78 +1,36 @@
-use std::{fmt, io, net::AddrParseError, str::Utf8Error};
+use std::{io, net::AddrParseError, str::Utf8Error};
 
 use crate::response::Response;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 /// The Error type
 pub enum Error {
     /// Occurred by UDP socket access
-    IO(io::Error),
+    #[error("io error: {0}")]
+    IO(#[from] io::Error),
     /// Occurred by std::str::from_utf8
-    Utf8(Utf8Error),
+    #[error("utf8 decoding error: {0}")]
+    Utf8(#[from] Utf8Error),
     /// HTTP request error
-    RequestError(reqwest::Error),
+    #[error("reqest error: {0}")]
+    RequestError(#[from] reqwest::Error),
     /// Bad HTTP Status Code
+    #[error("http responded with non-200 status code: {0}")]
     HTTPError(reqwest::StatusCode),
     /// Serialize, Deserialize failure
-    JSONError(serde_json::Error),
+    #[error("json error: {0}")]
+    JSONError(#[from] serde_json::Error),
     /// Bad Response Status Code (rsc)
+    #[error("api responded with non-200x rsc: {0:?}")]
     RSCError(Vec<Response>),
     /// IPv4 Address parse failure
-    AddrParseError(AddrParseError),
+    #[error("parse ip address error: {0}")]
+    AddrParseError(#[from] AddrParseError),
     /// Missing Property
+    #[error("no property found error")]
     NoProperty,
     /// HAP Server error
-    HAPError(hap::Error),
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Error::IO(err) => write!(f, "io error: {}", err),
-            Error::Utf8(err) => write!(f, "utf8 decoding error: {}", err),
-            Error::RequestError(err) => write!(f, "reqest error: {}", err),
-            Error::HTTPError(err) => write!(f, "http responded with non-200 status code: {}", err),
-            Error::JSONError(err) => write!(f, "json error: {}", err),
-            Error::RSCError(err) => write!(f, "api responded with non-200x rsc: {:?}", err),
-            Error::AddrParseError(err) => write!(f, "parse ip address error: {}", err),
-            Error::NoProperty => write!(f, "no property found error"),
-            Error::HAPError(err) => write!(f, "hap server error: {}", err),
-        }
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Self {
-        Error::IO(err)
-    }
-}
-
-impl From<Utf8Error> for Error {
-    fn from(err: Utf8Error) -> Self {
-        Error::Utf8(err)
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(err: serde_json::Error) -> Self {
-        Error::JSONError(err)
-    }
-}
-
-impl From<reqwest::Error> for Error {
-    fn from(err: reqwest::Error) -> Self {
-        Error::RequestError(err)
-    }
-}
-
-impl From<AddrParseError> for Error {
-    fn from(err: AddrParseError) -> Self {
-        Error::AddrParseError(err)
-    }
-}
-
-impl From<hap::Error> for Error {
-    fn from(err: hap::Error) -> Self {
-        Error::HAPError(err)
-    }
+    #[error("hap server error: {0}")]
+    HAPError(#[from] hap::Error),
 }
