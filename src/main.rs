22557a74061669e3574e0a9639cf3abcd6fa5e5d
Yuki MIZUNO <mizuyuu0904@gmail.com>
2025-07-05 19:23:13 +0900

    Support multiple devices

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use clap::{crate_authors, crate_description, crate_name, crate_version, Parser};
-use daikin_homekit::{characteristic::setup_characteristic, daikin::Daikin};
+use daikin_homekit::{characteristic::setup_characteristic, daikin::Daikin, info::DaikinInfo};
 use futures::{pin_mut, prelude::*};
 use log::{info, warn};
-use std::{net::Ipv4Addr, str::FromStr};
+use std::net::Ipv4Addr;
 
 use hap::{
     accessory::{
@@ -11,7 +11,7 @@ use hap::{
     },
     server::{IpServer, Server},
     storage::{FileStorage, Storage},
-    Config, MacAddress, Pin,
+    Config, Pin,
 };
 
 #[derive(Parser)]
@@ -24,7 +24,7 @@ use hap::{
 struct Cli {
     /// IPv4 address of Daikin AC
     #[arg(value_name = "ip_address")]
-    ip_addr: Option<String>,
+    ip_addrs: Vec<String>,
 }
 
 #[tokio::main]
@@ -35,24 +35,42 @@ async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
     let cli = Cli::parse();
-    let (daikin, info) = match cli.ip_addr {
-        Some(ip_addr) => {
-            let ip_addr = ip_addr.parse::<Ipv4Addr>()?;
-            let daikin = Daikin::new(ip_addr);
 
-            let info = daikin.get_info().await?;
-            (daikin, info)
-        }
-        None => {
-            let timeout = std::time::Duration::new(3, 0);
-            let stream = Daikin::discovery(timeout).await?;
-            pin_mut!(stream);
-            stream
-                .next()
-                .await
-                .ok_or(daikin_homekit::error::Error::Unknown)??
-        }
+    let devices: Vec<(Daikin, DaikinInfo)> = if !cli.ip_addrs.is_empty() {
+        info!("Using provided IP address(es): {:?}", cli.ip_addrs);
+
+        stream::iter(cli.ip_addrs)
+            .then(|ip| async move {
+                let ip_addr = ip.parse::<Ipv4Addr>()?;
+                let daikin = Daikin::new(ip_addr);
+                let info = daikin.get_info().await?;
+                anyhow::Ok((daikin, info))
+            })
+            .try_collect::<Vec<_>>()
+            .await?
+    } else {
+        warn!("No IP address provided. Discovering Daikin devices on the local network...");
+        let timeout = std::time::Duration::new(3, 0);
+        let stream = Daikin::discovery(timeout).await?;
+        pin_mut!(stream);
+        stream
+            .collect::<Vec<_>>()
+            .await
+            .into_iter()
+            .filter_map(|res| match res {
+                Ok((daikin, info)) => Some((daikin, info)),
+                Err(e) => {
+                    warn!("Error during discovery: {}", e);
+                    None
+                }
+            })
+            .collect()
     };
+
+    if devices.is_empty() {
+        warn!("No Daikin devices found. Exiting.");
+        return Ok(());
+    }
 
     let bridge = BridgeAccessory::new(
         1,
@@ -89,8 +107,6 @@ async fn main() -> anyhow::Result<()> {
             let config = Config {
                 pin: Pin::new([2, 0, 2, 3, 0, 4, 2, 0])?,
                 name: "Daikin Bridge".into(),
-                device_id: MacAddress::from_str(&info.mac().unwrap_or("000000000000".into()))
-                    .unwrap(),
                 category: AccessoryCategory::AirConditioner,
                 ..Default::default()
             };
@@ -102,19 +118,23 @@ async fn main() -> anyhow::Result<()> {
     let server = IpServer::new(config, storage).await?;
     server.add_accessory(bridge).await?;
 
-    let mut ac = HeaterCoolerAccessory::new(
-        info.edid().unwrap_or(2),
-        AccessoryInformation {
-            name: info.name().unwrap_or("Unknown name".into()),
-            manufacturer: "Daikin Industries, Ltd.".into(),
-            serial_number: info.mac().unwrap_or("000000000000".into()),
-            // WARNING: DO NOT COMMENT OUT BELOW
-            // firmware_revision: info.version(),
-            ..Default::default()
-        },
-    )?;
-    setup_characteristic(daikin, &mut ac.heater_cooler).await?;
-    server.add_accessory(ac).await?;
+    let mut index = 2u64;
+    for (daikin, info) in &devices {
+        let mut ac = HeaterCoolerAccessory::new(
+            info.edid().unwrap_or(index),
+            AccessoryInformation {
+                name: info.name().unwrap_or("Unknown name".into()),
+                manufacturer: "Daikin Industries, Ltd.".into(),
+                serial_number: info.mac().unwrap_or("000000000000".into()),
+                // WARNING: DO NOT COMMENT OUT BELOW
+                // firmware_revision: info.version(),
+                ..Default::default()
+            },
+        )?;
+        setup_characteristic(daikin.clone(), &mut ac.heater_cooler).await?;
+        server.add_accessory(ac).await?;
+        index += 1;
+    }
 
     let handle = server.run_handle();
 
