b88771e0bc70bcb6a5d098674c668759b00eb96f
Yuki MIZUNO <mizuyuu0904@gmail.com>
2026-04-22 01:05:06 +0900

    Replace homekit workspace with matter skeleton

--- homekit/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod characteristic;
-pub mod fan_mapping;
-pub mod mode_mapping;
-#[macro_use]
-extern crate log;
-
-// Re-export from daikin-client for convenience
-pub use daikin_client::{Daikin, DaikinInfo, DaikinStatus, HttpClient, ReqwestClient, discovery};
